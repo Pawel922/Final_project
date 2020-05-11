3ddd78343fc06223a7ce671cf04873065fb80b52
Pawel Wozniak <pawel.jan.wozniak@gmail.com>
2020-05-11 19:19:50 +0200

    SpringDataUserDetailsService was added

--- src/main/java/pl/coderslab/app/SecurityConfig.java
+++ src/main/java/pl/coderslab/app/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import pl.coderslab.service.SpringDataUserDetailsService;
 
 @Configuration
 @EnableWebSecurity
@@ -13,5 +14,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SpringDataUserDetailsService customUserDetailsService() {
+        return new SpringDataUserDetailsService();
     }
 }
