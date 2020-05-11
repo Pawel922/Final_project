c771e025d180bf43282166555d1bdb6964697629
Pawel Wozniak <pawel.jan.wozniak@gmail.com>
2020-05-11 22:01:33 +0200

    SecurityConfig was changed

--- src/main/java/pl/coderslab/app/SecurityConfig.java
+++ src/main/java/pl/coderslab/app/SecurityConfig.java
@@ -2,6 +2,7 @@ package pl.coderslab.app;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -19,5 +20,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     public SpringDataUserDetailsService customUserDetailsService() {
         return new SpringDataUserDetailsService();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/home").permitAll()
+                .antMatchers("/delivery/**").authenticated()
+                .and().formLogin();
     }
 }
