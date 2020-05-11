fde6d079d7776161b8226b8e996dcd2c031903c1
Pawel Wozniak <pawel.jan.wozniak@gmail.com>
2020-05-11 21:59:36 +0200

    New action to save user in database was added

--- src/main/java/pl/coderslab/controller/UserController.java
+++ src/main/java/pl/coderslab/controller/UserController.java
@@ -3,16 +3,31 @@ package pl.coderslab.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import pl.coderslab.entity.User;
+import pl.coderslab.repository.UserService;
 
 @Controller
 @RequestMapping(value = "/user", produces = "text/plain; charset=utf-8")
 public class UserController {
 
+    private final UserService userService;
+
+    public UserController (UserService userService) {
+        this.userService = userService;
+    }
+
     @GetMapping("/register")
     public String displayFormToRegisterNewUser(Model model) {
         model.addAttribute("user", new User());
         return "user-register";
+    }
+
+    @PostMapping("/register")
+    public String processFormToRegisterNewUser(@ModelAttribute User user) {
+        userService.saveUser(user);
+        return "/home";
     }
 }
